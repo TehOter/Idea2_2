661c65f8f7d38e9c663818c97466640c8b8f46b4
Игорь Романов <till5357@gmail.com>
2022-02-18 20:56:56 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+//вхожные данные
+        int balance = 10;
+        int amount = 1099;
+        int bonus = amount / 100;
+
+//Логика
+        if (amount >= 1000){
+            System.out.println(" Ваш Бонус " + (bonus));
+            System.out.println( " Ваш Счет " + (balance + amount + bonus));}
+        else {
+        System.out.println( " Ваш Счет " + (balance + amount));}
+
+
+
+
+    }
+}
